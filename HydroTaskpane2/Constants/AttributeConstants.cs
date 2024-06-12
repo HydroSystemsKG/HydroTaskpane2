a2211e4cd29e6688b3597c8f7707d405f29ed62e
HydroSystemsKG <andreas.menzel@hydro.aero>
2024-06-12 11:55:08 +0200

    dynamically built treeView

---
+++ HydroTaskpane2/Constants/AttributeConstants.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HydroTaskpane2.Constants
+{
+    public class AttributeConstants
+    {
+        public static Dictionary<string, List<string>> partDict = new Dictionary<string, List<string>>
+        {
+            {"Descriptions", new List<string>{ "Description", "Additional Description", "CAD Description"} },
+            {"Material", new List<string>{ "Material Hydro Variant 1", "Material Hydro Variant 2", "Material Hydro Additional Text", "Material Original", "Material Original Additional Text" } },
+            {"Treatments", new List<string>{ "Heat Treatment", "Preconditioning", "Surface Finish"} },
+            {"Color", new List<string>{ "Standard Color", "Lacquering"} },
+            {"Additional Information", new List<string>{ "Additional Information", "Based On", "DIN ISO 2768 | General Tolerances", "ISO 9013 | Separations Methods Precision", "Semifinished Standard"} },
+            {"Welding", new List<string>{ "Weldment", "Dye Penetration Inspection acc. ISO 3452-1", "ISO 17637 | Visual Inspection", "DIN EN ISO 5817 | Quality Level for Imperfection"} },
+            {"Dimensions", new List<string>{ "Measurements", "Weight"} }
+        };
+
+        public static Dictionary<string, List<string>> assemblyDict = new Dictionary<string, List<string>>
+        {
+            {"Descriptions", new List<string>{ "Description", "Additional Text", "CAD Description"} },
+            {"Treatments", new List<string>{ "Heat Treatment", "Preconditioning", "Surface Finish"} },
+            {"Color", new List<string>{ "Standard Color", "Lacquering"} },
+            {"Additional Information", new List<string>{ "Additional Information", "Based On", "DIN ISO 2768 | General Tolerances", "ISO 9013 | Separations Methods Precision", "Semifinished Standard"} },
+            {"Welding", new List<string>{ "Welding Assembly", "Dye Penetration Inspection acc. ISO 3452-1", "ISO 17637 | Visual Inspection", "DIN EN ISO 5817 | Quality Level for Imperfection"} },
+            {"Dimensions", new List<string>{ "Measurements", "Weight", "Components"} }
+        };
+
+        public static Dictionary<string, List<string>> drawingDict = new Dictionary<string, List<string>>
+        {
+            {"Changes", new List<string>{ "Change-No.", "Change Short Description", "Occurs", "Additional Information"} },
+            {"Treatments", new List<string>{ "Heat Treatment", "Preconditioning", "Surface Finish"} },
+            {"Color", new List<string>{ "Standard Color", "Lacquering"} },
+            {"Additional Information", new List<string>{ "Based On", "DIN ISO 2768 | General Tolerances", "ISO 9013 | Separations Methods Precision", "Semifinished Standard"} },
+            {"Welding", new List<string>{ "Welding Assembly", "Dye Penetration Inspection acc. ISO 3452-1", "ISO 17637 | Visual Inspection", "DIN EN ISO 5817 | Quality Level for Imperfection"} },
+            {"General", new List<string>{ "Drafter", "Date", "Weight", "Material"} }
+        };
+
+    }
+}
--- HydroTaskpane2/HydroTaskpane.xaml.cs
+++ HydroTaskpane2/HydroTaskpane.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
+using HydroTaskpane2.Constants;
 using System.Collections.ObjectModel;
 
 namespace HydroTaskpane2
@@ -25,8 +26,93 @@ namespace HydroTaskpane2
         public MainWindow()
         {
             InitializeComponent();
+
+            // populate treeview
+            populateTree();
+        }
+
+
+        #region TreeView methods and classes
+        public void populateTree()
+        {
+            List<AttributeGroup> groups = new List<AttributeGroup>();
+            Dictionary<string, Dictionary<string, List<string>>> groupReference = new Dictionary<string, Dictionary<string, List<string>>>
+            {
+                {"Part", AttributeConstants.partDict},
+                {"Assembly", AttributeConstants.assemblyDict},
+                {"Drawing", AttributeConstants.drawingDict}
+            };
+
+            foreach (string groupName in groupReference.Keys)
+            {
+                AttributeGroup refGroup = new AttributeGroup() { name = groupName };
+                refGroup.groups = new ObservableCollection<AttributeGroup>();
+
+                foreach (string key in groupReference[groupName].Keys)
+                {
+                    AttributeGroup currentGroup = new AttributeGroup() { name = key };
+                    List<string> attributeList = groupReference[groupName][key];
+
+                    foreach (string attr in attributeList)
+                    {
+                        currentGroup.attributes.Add(new AttributeField() { name = attr }); // add attributes after list is concluded
+                    }
+
+                    refGroup.groups.Add(currentGroup);
+                }
+
+                groups.Add(refGroup);
+            }
+
+            AttributeGroups.ItemsSource = groups;
+
         }
         
     }
 
+    public class AttributeGroup
+    {
+        public string name { get; set; }
+        public ObservableCollection<AttributeField> attributes { get; set; }
+        public ObservableCollection<AttributeGroup> groups { get; set; }
+
+        public IList<object> Items
+        {
+            get
+            {
+                IList<object> childNodes = new List<object>();
+                if (this.groups == null)
+                {
+                    foreach (var attr in this.attributes)
+                        childNodes.Add(attr);
+                }
+                else if (this.attributes == null)
+                {
+                    foreach (var group in this.groups)
+                        childNodes.Add(group);
+                }
+                else
+                {
+                    foreach (var group in this.groups)
+                        childNodes.Add(group);
+                    foreach (var attr in this.attributes)
+                        childNodes.Add(attr);
+                }
+
+                return childNodes;
+            }
+        }
+
+        public AttributeGroup()
+        {
+            this.attributes = new ObservableCollection<AttributeField>();
+        }
+    }
+
+    public class AttributeField
+    {
+        public string name { get; set; }
+        public string attribute { get; set; }
+    }
+    #endregion
 }
