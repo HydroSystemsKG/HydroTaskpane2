7b22d73ec0c509c9d404397f30ccf03acff1485a
HydroSystemsKG <andreas.menzel@hydro.aero>
2024-06-27 10:22:04 +0200

    added new cs file

---
+++ HydroTaskpane2/HydroTaskpane2_AddIn.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HydroTaskpane2
+{
+    public class HydroTaskpane2_AddIn
+    {
+    }
+}
